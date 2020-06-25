b8567613ff75b093ca8399a23819e821e15b05dd
jenkins mulbah <jenkinsmulbah6@gmail.com>
2020-06-25 12:53:52 +0000

    quiz app project

---
+++ app.js
@@ -0,0 +1,144 @@
+
+(function () {
+    function buildQuiz() {
+        //   variables to store  the html output
+        const output = [];
+
+        // for each question...
+        myQuestions.forEach((currentQuestion, questionNumber) => {
+            //variable to store the list of possible answers
+            const answers = [];
+
+            //and for each available answer..
+            for (letter in currentQuestion.answers) {
+                //... add an html radio button
+                answers.push(
+                    `<label>
+                       <input type="radio" name="question${questionNumber}" value="${letter}">
+                       ${letter} :
+                       ${currentQuestion.answers[letter]}
+                    </label>`
+                );
+            }
+            //  add this question and its answers to the output
+            output.push(
+                `<div class="slide">
+                   <div class="question"> ${currentQuestion.question}</div>
+                   <div class="answers"> ${answers.join('')}</div>
+                </div>`
+            );
+        }
+        );
+        //finally combine the output list into one string of HTML and put it on the page
+        quizContainer.innerHTML = output.join('')
+    }
+    function showResults() {
+
+        // gather answer containers from our quiz
+        const answerContainers = quizContainer.querySelectorAll('.answers');
+
+        // keep track of user's answers
+        let numCorrect = 0;
+
+        // for each question...
+        myQuestions.forEach((currentQuestion, questionNumber) => {
+
+            // find selected answer
+            const answerContainer = answerContainers[questionNumber];
+            const selector = `input[name=question${questionNumber}]:checked`;
+            const userAnswer = (answerContainer.querySelector(selector) || {}).value;
+
+            // if answer is correct
+            if (userAnswer === currentQuestion.correctAnswer) {
+                // add to the number of correct answers
+                numCorrect++;
+
+                // color the answers green
+                answerContainers[questionNumber].style.color = 'lightgreen';
+            }
+            // if answer is wrong or blank
+            else {
+                // color the answers red
+                answerContainers[questionNumber].style.color = 'red';
+            }
+        });
+
+        // show number of correct answers out of total
+        resultsContainer.innerHTML = `${numCorrect} out of ${myQuestions.length}`;
+    }
+
+    function showSlide(n) {
+        slides[currentSlide].classList.remove('active-slide');
+        slides[n].classList.add('active-slide');
+        currentSlide = n;
+        if (currentSlide === 0) {
+            previousBtn.style.color = 'red';
+        }
+        else {
+            previousBtn.style.display = 'inline-block';
+        }
+        if (currentSlide === slides.length - 1) {
+            nextBtn.style.display = 'none';
+            submitButton.style.display = 'inline-block';
+        }
+        else {
+            nextBtn.style.display = 'inline-block';
+            submitButton.style.display = 'none';
+        }
+    }
+
+
+    function showNextSlide() {
+        showSlide(currentSlide + 1);
+    }
+    function showPreviousSlide() {
+        showSlide(currentSlide - 1);
+    }
+    //Variables
+    const quizContainer = document.getElementById('quiz');
+    const resultsContainer = document.getElementById('result')
+    const submitButton = document.getElementById('submit');
+    const myQuestions = [
+        {
+            question: "Who invented JavaScript?",
+            answers: {
+                a: "Douglas Crockford",
+                b: "Sheryl Sandberg",
+                c: "Brendan Eich"
+            },
+            correctAnswer: "c"
+        },
+        {
+            question: "Which one of these is a JavaScript package manager?",
+            answers: {
+                a: "Node.js",
+                b: "TypeScript",
+                c: "npm"
+            },
+            correctAnswer: "c"
+        },
+        {
+            question: "Which tool can you use to ensure code quality?",
+            answers: {
+                a: "Angular",
+                b: "jQuery",
+                c: "RequireJS",
+                d: "ESLint"
+            },
+            correctAnswer: "d"
+        }
+    ];
+    buildQuiz();
+    // navigation
+
+    const previousBtn = document.getElementById("pre");
+    const nextBtn = document.getElementById("next");
+    const slides = document.querySelectorAll(".slide");
+    let currentSlide = 0;
+
+    showSlide(currentSlide)
+
+    submitButton.addEventListener('click', showResults);
+    previousBtn.addEventListener('click', showPreviousSlide);
+    nextBtn.addEventListener('click', showNextSlide);
+})()
---
+++ script.js
@@ -0,0 +1,168 @@
+(function(){
+    // Functions
+    function buildQuiz(){
+      // variable to store the HTML output
+      const output = [];
+  
+      // for each question...
+      myQuestions.forEach(
+        (currentQuestion, questionNumber) => {
+  
+          // variable to store the list of possible answers
+          const answers = [];
+  
+          // and for each available answer...
+          for(letter in currentQuestion.answers){
+  
+            // ...add an HTML radio button
+            answers.push(
+              `<label>
+                <input type="radio" name="question${questionNumber}" value="${letter}">
+                ${letter} :
+                ${currentQuestion.answers[letter]}
+              </label>`
+            );
+          }
+  
+          // add this question and its answers to the output
+          output.push(
+            `<div class="slide">
+              <div class="question"> ${currentQuestion.question} </div>
+              <div class="answers"> ${answers.join("")} </div>
+            </div>`
+          );
+        }
+      );
+  
+      // finally combine our output list into one string of HTML and put it on the page
+      quizContainer.innerHTML = output.join('');
+    }
+  
+    function showResults(){
+  
+      // gather answer containers from our quiz
+      const answerContainers = quizContainer.querySelectorAll('.answers');
+  
+      // keep track of user's answers
+      let numCorrect = 0;
+  
+      // for each question...
+      myQuestions.forEach( (currentQuestion, questionNumber) => {
+  
+        // find selected answer
+        const answerContainer = answerContainers[questionNumber];
+        const selector = `input[name=question${questionNumber}]:checked`;
+        const userAnswer = (answerContainer.querySelector(selector) || {}).value;
+  
+        // if answer is correct
+        if(userAnswer === currentQuestion.correctAnswer){
+          // add to the number of correct answers
+          numCorrect++;
+  
+          // color the answers green
+          answerContainers[questionNumber].style.color = 'lightgreen';
+        }
+        // if answer is wrong or blank
+        else{
+          // color the answers red
+          answerContainers[questionNumber].style.color = 'red';
+        }
+      });
+       //show level 
+        if(numCorrect == 0){
+           resultCode.innerHTML = "FAIL";
+           resultCode.style.color = "red"
+        }else if(numCorrect < myQuestions.length){
+            resultCode.innerHTML = "TRY"
+           resultCode.style.color = "lightgreen"
+
+        }else{
+          resultCode.innerHTML = "Genius";
+          resultCode.style.color = "blue";
+        }
+      // show number of correct answers out of total
+      resultsContainer.innerHTML = `${numCorrect} out of ${myQuestions.length}`;
+    }
+  
+    function showSlide(n) {
+      slides[currentSlide].classList.remove('active-slide');
+      slides[n].classList.add('active-slide');
+      currentSlide = n;
+      if(currentSlide === 0){
+        previousButton.style.display = 'none';
+      }
+      else{
+        previousButton.style.display = 'inline-block';
+      }
+      if(currentSlide === slides.length-1){
+        nextButton.style.display = 'none';
+        submitButton.style.display = 'inline-block';
+      }
+      else{
+        nextButton.style.display = 'inline-block';
+        submitButton.style.display = 'none';
+      }
+    }
+  
+    function showNextSlide() {
+      showSlide(currentSlide + 1);
+    }
+  
+    function showPreviousSlide() {
+      showSlide(currentSlide - 1);
+    }
+  
+    // Variables
+    const quizContainer = document.getElementById('quiz');
+    const resultsContainer = document.getElementById('results');
+    const submitButton = document.getElementById('submit');
+    const resultCode = document.getElementById('resultCode');
+    const myQuestions = [
+      {
+        question: "Who was the first President of Liberia?",
+        answers: {
+          a: "Charles DB King",
+          b: "Gorge M weah",
+          c: "Joseph Jenkins Roberts"
+        },
+        correctAnswer: "c"
+      },
+      {
+        question: "When did Liberia gain Independence? ",
+        answers: {
+          a: "1884",
+          b: "1850",
+          c: "1847"
+        },
+        correctAnswer: "c"
+      },
+      {
+        question: "Who was the first female President of Liberia?",
+        answers: {
+          a: "Musu Harris",
+          b: "Comfort Mulbah",
+          c: "Hawa Morris",
+          d: "Ellen Johnson Sirleaf"
+        },
+        correctAnswer: "d"
+      }
+    ];
+  
+    // Kick things off
+    buildQuiz();
+  
+    // Pagination
+    const previousButton = document.getElementById("previous");
+    const nextButton = document.getElementById("next");
+    const slides = document.querySelectorAll(".slide");
+    let currentSlide = 0;
+  
+    // Show the first slide
+    showSlide(currentSlide);
+  
+    // Event listeners
+    submitButton.addEventListener('click', showResults);
+    previousButton.addEventListener("click", showPreviousSlide);
+    nextButton.addEventListener("click", showNextSlide);
+  })();
+  
